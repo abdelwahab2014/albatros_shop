88102ea408cdda9f197f9d1531abdc572bd9c51b
Abdelwahab <data28448802@gmail.com>
2022-10-05 13:41:05 +0200

    SignIn-LogIn form

---
+++ lib/login.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: const EdgeInsets.only(left: 40, right: 40),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: height * 0.04),
+              const Text(
+                "Welcome in our shop ",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color(0xFF363f93),
+                ),
+              ),
+              const Text(
+                "Here you can have chats and discuss with us",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color(0xFF363f93),
+                ),
+              ),
+              SizedBox(height: height * 0.05),
+              SizedBox(height: height * 0.05),
+              TextFormField(
+                decoration:
+                    const InputDecoration(labelText: "Enter Your Phone Number"),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      !RegExp(r'(^(?:[+0]9)?[0-9]{10,12}$)').hasMatch(value)) {
+                    return 'Please Enter vaild Number';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: height * 0.05),
+              TextFormField(
+                decoration:
+                    const InputDecoration(labelText: "Enter Your Email"),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                    return 'Please Enter Vaild Email';
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Processing Your Data')),
+                          );
+                        }
+                      },
+                      child: const Text("LogIn"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Processing Your Data')),
+                          );
+                        }
+                      },
+                      child: const Text("SignUp"),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,37 +1,5 @@
 import 'package:flutter/material.dart';
 
-class Message extends StatefulWidget {
-  const Message({super.key});
-
-  @override
-  State<Message> createState() => _MessageState();
-}
-
-class _MessageState extends State<Message> {
-  //final _formKey = GlobalKey<FormState>();
-
-  bool index = true;
-  List<Widget> pages = const [
-    SignUp(),
-    Login(),
-  ];
-  List<String> sl = ['SignUp', 'LogIn'];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: index == true ? pages[0] : pages[1],
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            index = !index;
-          });
-        },
-        child: index == true ? Text(sl[1]) : Text(sl[0]),
-      ),
-    );
-  }
-}
-
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
 
@@ -125,96 +93,11 @@ class _SignUpState extends State<SignUp> {
                       child: const Text("SignUp"),
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Login extends StatefulWidget {
-  const Login({super.key});
-
-  @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  @override
-  Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      key: _scaffoldKey,
-      backgroundColor: Colors.white,
-      body: Container(
-        padding: const EdgeInsets.only(left: 40, right: 40),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: height * 0.04),
-              const Text(
-                "Welcome in our shop ",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Color(0xFF363f93),
-                ),
-              ),
-              const Text(
-                "Here you can have chats and discuss with us",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Color(0xFF363f93),
-                ),
-              ),
-              SizedBox(height: height * 0.05),
-              SizedBox(height: height * 0.05),
-              TextFormField(
-                decoration:
-                    const InputDecoration(labelText: "Enter Your Phone Number"),
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      !RegExp(r'(^(?:[+0]9)?[0-9]{10,12}$)').hasMatch(value)) {
-                    return 'Please Enter vaild Number';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: height * 0.05),
-              TextFormField(
-                decoration:
-                    const InputDecoration(labelText: "Enter Your Email"),
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(value)) {
-                    return 'Please Enter Vaild Email';
-                  }
-                  return null;
-                },
-              ),
-              Row(
-                children: [
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 16.0),
                     child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text('Processing Your Data')),
-                          );
-                        }
-                      },
-                      child: const Text("LogIn"),
+                      onPressed: () {},
+                      child: const Text("LogIn tests"),
                     ),
                   ),
                 ],
