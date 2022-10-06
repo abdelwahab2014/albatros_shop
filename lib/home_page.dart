adf072baccc482f85f09f61f243f3e1160c8eab5
Abdelwahab <data28448802@gmail.com>
2022-10-06 15:34:33 +0200

    ListView Builder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children:  [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.home),
-          ),
-        ],
+    return Scaffold(
+      body: Container(
+        height: 200,
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          separatorBuilder: ((context, index) => const SizedBox(
+                width: 12,
+              )),
+          itemBuilder: (context, index) => productCard(),
+        ),
       ),
     );
   }
 }
+
+Widget productCard() => Container(
+      width: 200,
+      height: 200,
+      color: Colors.red,
+      child: const Text("Product"),
+    );
